409c4b40ee4bfeb3a3780848274e43d4f70b31a3
Jasminecr <eseose001@gmail.com>
2023-02-20 17:58:27 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: 'Print numbers of base 16'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++);
+	{
+		printf("%d", n);
+	}
+
+	for (m = 97; m <= 102; m++);
+	{
+		printf("%d", m);
+	}
+	printf("\n");
+	return (0);
+}
